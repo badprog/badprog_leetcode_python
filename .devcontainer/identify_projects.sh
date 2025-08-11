2600b4046f2d6ff1af3a69a3c95a37b1d4e51fa5
Mi-K <badprog@users.noreply.github.com>
2025-08-11 17:52:56 +0200

    Updating.

--- .devcontainer/identify_projects.sh
+++ .devcontainer/identify_projects.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # List every folder as p_[0-9]*_*
-for project_dir in /workspace/p_[0-9]*_*; do
+for project_dir in p_[0-9]*_*; do
     if [ -d "$project_dir" ]; then
-        echo "$project_dir"
+        echo "$PWD/$project_dir"
     fi
 done
