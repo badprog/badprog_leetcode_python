2ae6d52737e18808035ab6ecad9557e88b025047
Mi-K <badprog@users.noreply.github.com>
2025-08-22 19:03:01 +0200

    Updating sys.path.

--- p_3_longest_substring_without_repeating_characters/tests/test_solution.py
+++ p_3_longest_substring_without_repeating_characters/tests/test_solution.py
@@ -1,5 +1,13 @@
 # https://github.com/badprog/badprog_leetcode_python
 
+############################
+# import
+import sys
+import pathlib
+
+# Adding the parent directory of this file to the sys.path
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
 from src.solution import Solution 
 
 class TestP3LongestSubstringWithoutRepeatingCharacters():
